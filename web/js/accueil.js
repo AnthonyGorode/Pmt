0baecace11ef796c5b5b2d79ae5fc7b0ca2f7aa4
Anthony Gorode <anthony.gorode@gmail.com>
2019-10-26 02:45:48 +0200

    page accueil : mise en place de la fonctionnalité show/hide pour la première section, la bordure depasse et atteint la sous-section

--- web/js/accueil.js
+++ web/js/accueil.js
@@ -1,72 +1,30 @@
-submitResetPassword = () => {
-    let isCodeValid = false;
-    let el = $('#error-display');
-    let aOfDatas = [
-        {type : 'email', val : $('#formateur_mail_forgot').val().toLowerCase()},
-    ];
-    let isValid = new RegExp('^[0-9a-z._-]+@{1}[0-9a-z.-]{2,}[.]{1}[a-z]{2,5}$', 'i');
-    isCodeValid = isValid.test(aOfDatas[0].val);
-
-    if(!isCodeValid) {
-        $('#formateur_mail_forgot').css('border', '2px solid red');
-        $('.modal-info').text('*L\'email n\'est pas valide').css('color', 'red');
-    } else {
-        console.log('launch ajax')
-        ajaxForReinitializeMail();
+const CSS = {
+    show_carre_absolute: {
+        "border":"1px solid #7fc241",
+        "border-top-color": "#fff",
+        "border-bottom-color": "#fff",
+        "background": "white"
+    },
+    hide_carre_absolute: {
+        "border":"1px solid transparent",
+        "border-top-color": "transparent",
+        "border-bottom-color": "transparent",
+        "background": "transparent"
     }
-};
-
-/**
- * CHECK VALIDITY of the code provided by user
- *   Return boolean
- */
-const isCodeFormValid = () => {
-    let isCodeValid = false;
-    let el = $('#error-display');
-    let aOfDatas = [
-        {type : 'email', val : $('#formateur-email').val().toLowerCase()},
-        {type : 'password', val : $('#formateur-password').val().toLowerCase()}
-    ];
-
-
-    aOfDatas.map(data => {
-        switch(data.type) {
-            case "email":
-                let isValid = new RegExp('^[0-9a-z._-]+@{1}[0-9a-z.-]{2,}[.]{1}[a-z]{2,5}$', 'i');
-                isCodeValid = isValid.test(data.val);
-
-                break;
-            case "password":
-                (data.val !== "") ? isCodeValid = true : isCodeValid = false;
-                break;
-            default:
-                break
-        }
-    });
-
-    if(!isCodeValid) {
-        el.parent().parent().find('.c-tooltip')
-            .addClass('invalid-tootip')
-            .html("Vos identifiants sont incorrects.")
-
-        setTimeout(()=> {
-            el.siblings('.c-tooltip')
-                .removeClass('invalid-tootip')
-                .html("")
-        }, 2000)
-    }
-    return isCodeValid
-
-};
-
-
-function resetInput() {
-    $('#formateur_mail_forgot').val('');
-    $('.modal-info').text('').css('color', 'red');
 }
 
-function displayPop() {
-    let element = $( "#index-info-pop" );
-    element.attr("class","w3-container w3-center w3-animate-left" ).show();
-    console.log('adding class for animate');
+const toggleCarre = (numIdCarre) => {
+    $("#absolute-section").children().map(function() {
+        let element = $(this).attr('id')
+        let ishow = $(this).attr('ishow')
+
+        if(element == `carre-${numIdCarre}` && !ishow){
+            $(this).attr('ishow','true')
+            $(`#${element}`).css(CSS.show_carre_absolute)
+        }
+        else {
+            $(this).attr('ishow','')
+            $(`#${element}`).css(CSS.hide_carre_absolute)
+        }
+    })
 }
